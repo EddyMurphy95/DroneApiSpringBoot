ce21312dbb9d14d2fa279f56881c38573132f81b
Nana Oduro <eddymurphy413@gmail.com>
2022-09-15 19:09:48 +0000

    initiaising project

---
+++ src/main/java/com/droneapi/com/droneapi/DroneapiApplication.java
@@ -0,0 +1,13 @@
+package com.droneapi.com.droneapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroneapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/droneapi/com/droneapi/DroneapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.droneapi.com.droneapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
